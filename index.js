fdb56168275687d3375da3d9795617250f16d157
timlin <timlin1972@gmail.com>
2020-11-29 14:30:19 +0800

    Refactor

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 const winston = require('winston');
 const { combine, printf } = winston.format;
 
+const MODULE_NAME = 'logger';
+
 const DEF_TO_CONSOLE = true;
 
 const DEF_CONFIGS = {
   toConsole: DEF_TO_CONSOLE,
-}
+};
 
 const DEF_MODULE_NAME = 'unknown';
 const DEF_LEVEL = 'info';
 
 const tlnFormat = printf(({ level, message }) => `${level}: ${message}`);
 
-function logger(configs=DEF_CONFIGS) {
-  const toConsole = configs.toConsole | DEF_TO_CONSOLE;
-
-  let logger = null;
-  if (toConsole) {
-    logger = winston.createLogger({
-      format: combine(
-        winston.format.colorize(),
-        tlnFormat,
-      ),
-      transports: [
-        new winston.transports.Console(),
-      ]
-    });
+class Logger {
+  constructor(configs=DEF_CONFIGS) {
+    this.toConsole = configs.toConsole | DEF_TO_CONSOLE;
+    this.logger = this.toConsole ?
+      winston.createLogger({
+        format: combine(
+          winston.format.colorize(),
+          tlnFormat,
+        ),
+        transports: [
+          new winston.transports.Console(),
+        ],
+      })
+      : null;
   }
 
-  this.log = (moduleName=DEF_MODULE_NAME, level=DEF_LEVEL, msg) => {
-    if (!logger)  return;
-    logger.log({
-      level,
-      message: `[${moduleName}] ${msg}`
-    })
+  log = (moduleName=DEF_MODULE_NAME, level=DEF_LEVEL, msg=null) => {
+    if (this.logger) {
+      this.logger.log({
+        level,
+        message: `[${moduleName}] ${msg}`
+      })
+    }
   }
 
+  toString = () => `[${MODULE_NAME}]\n\
+    \ttoConsole: ${this.toConsole}\n\
+    `;
 }
 
-module.exports = logger;
+module.exports = Logger;
